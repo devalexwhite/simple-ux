be31d87c552135a055de8f6a31813e5274f2d663
Alex White <alex.white@hey.com>
2021-11-22 20:55:59 -0500

    Fixing commit issue

--- pages/card-sort/congrats.js
+++ pages/card-sort/congrats.js
@@ -10,18 +10,18 @@ const CompletePage = () => {
         <article className="prose mx-auto">
           <h1>{congratsTitle}</h1>
           <p className="mb-6">{congratsBody}</p>
-          <div class="alert alert-info">
-            <div class="flex-1">
+          <div className="alert alert-info">
+            <div className="flex-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                class="w-6 h-6 mx-2 stroke-current"
+                className="w-6 h-6 mx-2 stroke-current"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
