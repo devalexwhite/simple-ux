c9d2bbb3b1982f690c982b1952029b7d940318bb
Alex White <alex.white@hey.com>
2021-11-23 15:00:44 -0500

    Added TailwindUI to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,71 @@
-import { ChevronRightIcon, StarIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 import { ReactSVG } from "react-svg";
+import { FeaturesRow } from "../components/featuresRow";
+import { Footer } from "../components/footer";
+import { SubscribedDialog } from "../components/modals/subscribedDialog";
 
 const Home = () => {
+  const [email, setEmail] = useState("");
+  const [emailModalOpen, setEmailModalOpen] = useState(false);
+  const [disableEmailButton, setDisableEmailButton] = useState(false);
+  const features = [
+    {
+      name: "Pay what you want",
+      text: "You choose what our products are worth to you.",
+    },
+    {
+      name: "Detailed analytics",
+      text: "All of our products provide analytical tools to help you understand your users.",
+    },
+    {
+      name: "Made for real people",
+      text: "Your users will love using our tools to participate in studies.",
+    },
+    {
+      name: "Seriously simple",
+      text: "We don't believe in complexity for the sake of it. ",
+    },
+    {
+      name: "Open source",
+      text: "We build in the open and are fully transparent.",
+    },
+    {
+      name: "Let us teach you",
+      text: "Visit our YouTube channel and blog for free educational resources.",
+    },
+  ];
+
+  const submitEmail = () => {
+    setDisableEmailButton(true);
+    try {
+      fetch("/api/capture-email", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      }).then((res) => {
+        if (res.status !== 200)
+          alert(
+            "An error has occurred, please try check your email address and try again."
+          );
+        else setEmailModalOpen(true);
+      });
+    } catch (e) {
+      alert(
+        "An error has occurred, please try check your email address and try again."
+      );
+    } finally {
+      setDisableEmailButton(false);
+    }
+  };
+
   return (
     <div className="bg-white pb-8 sm:pb-12 lg:pb-12">
+      <SubscribedDialog open={emailModalOpen} setOpen={setEmailModalOpen} />
       <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
         <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
           <div>
@@ -20,8 +82,8 @@ const Home = () => {
                   {"UX tools for any size team"}
                 </h1>
                 <p className="mt-6 text-xl text-gray-500">
-                  Easy-to-use UX tools and educational resources at
-                  name-your-own pricing. <strong>Coming soon.</strong>
+                  Pay what you want for easy-to-use UX tools and educational
+                  resources. <strong>Coming soon.</strong>
                 </p>
               </div>
               <form action="#" className="mt-12 sm:max-w-lg sm:w-full sm:flex">
@@ -34,12 +96,17 @@ const Home = () => {
                     type="email"
                     className="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                     placeholder="Enter your email"
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
                 <div className="mt-4 sm:mt-0 sm:ml-3">
                   <button
+                    disabled={disableEmailButton}
+                    onClick={submitEmail}
                     type="submit"
-                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                    className={`block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10 ${
+                      disableEmailButton && "pointer-not-allowed opacity-20"
+                    }`}
                   >
                     Notify me
                   </button>
@@ -86,15 +153,20 @@ const Home = () => {
                 />
               </svg>
             </div>
-            <div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12">
+            <div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0  lg:max-w-none lg:h-full lg:pl-12">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+                className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5  lg:h-full lg:w-auto lg:max-w-none"
                 src="/card-sort-screenshot.png"
                 alt="Screenshot of the card sorting tool created by SimpleUX"
               />
             </div>
           </div>
         </div>
+      </div>
+      <FeaturesRow features={features} />
+      <div className="mt-16">
+        <Footer />
       </div>
     </div>
   );
