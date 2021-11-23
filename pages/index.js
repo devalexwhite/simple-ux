61c58c2a9c6029e2e6ccce1b36d309fe240bb0cd
Alex White <alex.white@hey.com>
2021-11-23 13:01:37 -0500

    Tailwind UI wip

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,103 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Modal } from "../components/modal";
+import { ChevronRightIcon, StarIcon } from "@heroicons/react/solid";
+import { ReactSVG } from "react-svg";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="prose container mx-auto my-16 px-4">
-      <Head>
-        <title>SimpleUX - Simple, bloat-free UX tools</title>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <meta
-          name="description"
-          content="SimpleUX empowers UX professionals through a suite of bullshit-free tools and educational resources that are available at whatever price point you can afford."
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <header>
-        <div className="w-full  mb-16">
-          <img src="/logo.png" width="300" />
+    <div className="bg-white pb-8 sm:pb-12 lg:pb-12">
+      <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
+        <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
+          <div>
+            <div>
+              <ReactSVG
+                aria-label="SimpleUX logo with a square and triangle icon"
+                src="/logo.svg"
+                className="text-indigo-600 w-60"
+              />
+            </div>
+            <div className="mt-20">
+              <div className="mt-6 sm:max-w-xl">
+                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  {"UX tools for any size team"}
+                </h1>
+                <p className="mt-6 text-xl text-gray-500">
+                  Easy-to-use UX tools and educational resources at
+                  name-your-own pricing. <strong>Coming soon.</strong>
+                </p>
+              </div>
+              <form action="#" className="mt-12 sm:max-w-lg sm:w-full sm:flex">
+                <div className="min-w-0 flex-1">
+                  <label htmlFor="hero-email" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="hero-email"
+                    type="email"
+                    className="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div className="mt-4 sm:mt-0 sm:ml-3">
+                  <button
+                    type="submit"
+                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                  >
+                    Notify me
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <h1>{"Simple, bloat-free UX tools"}</h1>
-        <span>
-          {
-            "SimpleUX empowers UX professionals through a suite of bullshit-free tools and educational resources that are available at whatever price point you can afford. We operate transparently and our products are open-source."
-          }
-        </span>
-      </header>
-      <article>
-        <h2>{"SimpleUX is right around the corner"}</h2>
-        <p>
-          {
-            "We're not quite ready yet, but we've got a lot of exciting things in the oven. Until then, check out some of these links:"
-          }
-        </p>
-        <ul>
-          <li>
-            <a
-              href="https://world.hey.com/alex.white"
-              target="_blank"
-              rel="noreferrer"
-              className="link"
-            >
-              {"News and announcements"}
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/devalexwhite/simple-ux"
-              target="_blank"
-              rel="noreferrer"
-              className="link"
-            >
-              {"SimpleUX on GitHub"}
-            </a>
-          </li>
-        </ul>
-        <h2>{"Sponsor SimpleUX"}</h2>
-        <p>
-          {
-            "SimpleUX is built 100% in the open and supported entirely by our sponsors. Sponsoring SimleUX helps us pay infrastructure costs and continue to develop the tools SimpleUX offers."
-          }
-        </p>
-        <iframe
-          src="https://github.com/sponsors/devalexwhite/card"
-          title="Sponsor devalexwhite"
-          height="170"
-          width="600"
-          className="border-0"
-        ></iframe>
-        <h2>{"Questions or feedback?"}</h2>
-        <p>
-          {"We're very open to any feedback or questions. "}
-          <a href="mailto:alex.white@hey.com">Click here</a>
-          {
-            " to send us an email and we'll get back to you within a couple of days."
-          }
-        </p>
-      </article>
+
+        <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
+          <div className="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <div className="hidden sm:block">
+              <div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
+              <svg
+                className="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
+                width={404}
+                height={392}
+                fill="none"
+                viewBox="0 0 404 392"
+              >
+                <defs>
+                  <pattern
+                    id="837c3e70-6c3a-44e6-8854-cc48c737b659"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={392}
+                  fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"
+                />
+              </svg>
+            </div>
+            <div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12">
+              <img
+                className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+                src="/card-sort-screenshot.png"
+                alt="Screenshot of the card sorting tool created by SimpleUX"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
